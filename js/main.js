36a7eb39a4262efdd2eef668dd6306e25220cd62
pshemcio <daglas92@gmail.com>
2021-02-03 13:49:01 +0100

    Finish burger menu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
+    initializeSplideSlider();
+
+    mainNav.addEventListener('click', showBurgerMenu);
+});
+
+const mainNav = document.getElementById('main-nav');
+
+const initializeSplideSlider = () => {
     new Splide('.header-splide', {
         type: 'fade',
         rewind: true,
@@ -15,4 +23,13 @@ document.addEventListener('DOMContentLoaded', function () {
             arrow: 'splide__arrow header-btns',
         },
     }).mount();
-});
+};
+
+const showBurgerMenu = () => {
+    const burgerBtn = mainNav.querySelector('.c-burger');
+    const menu = mainNav.querySelector('.l-main-nav');
+
+    document.body.classList.toggle('js-mobile-menu-open');
+    burgerBtn.classList.toggle('c-burger--active');
+    menu.classList.toggle('l-main-nav--active');
+};

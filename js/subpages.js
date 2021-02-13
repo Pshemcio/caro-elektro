49b3aca86791609b51e710c70d2a5936aa297790
pshemcio <daglas92@gmail.com>
2021-02-13 14:17:38 +0100

    Add resize on scroll effect to subpages header bg

--- js/subpages.js
+++ js/subpages.js
@@ -2,10 +2,24 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('scroll', handleScrollEvents);
+    window.addEventListener('resize', () => {
+        bgInitialSize = parseInt(window.getComputedStyle(subpage).getPropertyValue('background-size').slice(0, -1));
+    })
 });
+
+const subpage = document.querySelector('.subpage');
+let bgInitialSize = parseInt(window.getComputedStyle(subpage).getPropertyValue('background-size').slice(0, -1));
+
+const bgSizeChange = (section, speed) => {
+    console.log(width)
+    const deviceSpeed = (width / 100) * speed;
+
+    section.style.backgroundSize = `${bgInitialSize + (window.scrollY / deviceSpeed)}%`;
+};
 
 const handleScrollEvents = () => {
     const splitDelay = height;
 
     parallaxEffect(headerHero, 5, splitDelay);
+    bgSizeChange(subpage, 4);
 };

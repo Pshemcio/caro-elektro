2eb5a418c360fdc63cf0289d634ddf3c3c63416a
pshemcio <daglas92@gmail.com>
2021-02-18 10:23:13 +0100

    Fix subpages header readability issue

--- js/subpages.js
+++ js/subpages.js
@@ -14,19 +14,19 @@ const bgSizeChange = (section, speed) => {
     // const deviceSpeed = (width / 100) * speed;
     let deviceSpeed = speed;
     if (width > 1000) {
-        deviceSpeed = speed * 3;
+        deviceSpeed = speed * 3.5;
     }
 
     if (-1000 > section.getBoundingClientRect().bottom) {
         return;
     };
 
-    section.style.backgroundSize = `${bgInitialSize + (window.scrollY / deviceSpeed)}%`;
+    section.style.backgroundSize = `${bgInitialSize + (window.pageYOffset / deviceSpeed)}%`;
 };
 
 const handleScrollEvents = () => {
     const splitDelay = height;
 
     parallaxEffect(headerHero, 5, splitDelay);
-    bgSizeChange(subpage, 20);
+    bgSizeChange(subpage, 40);
 };
